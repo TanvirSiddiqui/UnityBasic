9253d74ef7de3c8f334ac4492e6abef28efdeb32
Md. Tanvir Siddiqui <babatanvir@hotmail.com>
2020-12-27 01:45:06 +0600

    Adding the whole project

---
+++ basic101/Assets/Scipts/basic.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class basic : MonoBehaviour
+{
+   
+    public float speed = 5.0f;
+    // Start is called before the first frame update
+    Rigidbody player; 
+    void Start()
+    {
+         player = GetComponent<Rigidbody>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+        print(player.name + "'s position: " + player.position);
+    }
+
+    public void Movement()
+    {
+        if (Input.GetKey("w"))
+        {
+            transform.Translate(transform.forward * speed * Time.deltaTime);
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.Translate(-transform.forward * speed * Time.deltaTime);
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.Translate(-transform.right * speed * Time.deltaTime);
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.Translate(transform.right * speed * Time.deltaTime);
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        print(collision.gameObject.name);
+    }
+
+}
